b7c5e267d548a3e39c6abdaedf865345219d1ffb
marcelocerqueira <50112732+marcelocerqueira69@users.noreply.github.com>
2020-03-29 18:59:53 +0100

    Versão 1.1

--- app/src/main/java/com/example/myownnotes/MainActivity.java
+++ app/src/main/java/com/example/myownnotes/MainActivity.java
@@ -1,20 +1,85 @@
 package com.example.myownnotes;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView anonymous;
+    EditText username;
+    EditText password;
+    CardView loginButton;
+    String URL="http://localhost:4000/user/login";
+
+    public void Submit(String userPassword){
+        final String savedata = userPassword;
+
+
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try{
+                    JSONObject objres = new JSONObject(response);
+                    Toast.makeText(getApplicationContext(), objres.toString(), Toast.LENGTH_SHORT).show();
+                } catch (JSONException e) {
+                    Toast.makeText(getApplicationContext(), "Server Error", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        loginButton = findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userPassword = "{ password: \'" + password.getText().toString() + "\', email: \'" + username.getText().toString() + "\'}";
+                Submit(userPassword);
+
+            }
+        });
+
+
+
 
         anonymous = findViewById(R.id.anonymous);
 
@@ -26,4 +91,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+
 }
+
+
